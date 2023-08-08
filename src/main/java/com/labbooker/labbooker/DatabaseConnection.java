156dc6735417c28ddcd1cc6e3532cd4a7d5c06a1
s-kvng <kvngnathan8420@gmail.com>
2023-08-08 20:26:39 +0000

    add database connection file

---
+++ src/main/java/com/labbooker/labbooker/DatabaseConnection.java
@@ -0,0 +1,27 @@
+package com.labbooker.labbooker;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+
+    public Connection databaseLink;
+
+    public Connection getConnection(){
+        String databaseName = "labBooker";
+        String databaseUser = "root";
+        String databasePassword = "";
+        String url = "jdbc:mysql://localhost:3306/" + databaseName;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url,databaseUser , databasePassword);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return databaseLink;
+    };
+}
+
