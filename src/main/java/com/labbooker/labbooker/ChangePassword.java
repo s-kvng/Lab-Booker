e53e34b2399f2dce2df4fc558a34194b6f09a65d
s-kvng <kvngnathan8420@gmail.com>
2023-08-10 20:04:32 +0000

    handle errors in resetting password

--- src/main/java/com/labbooker/labbooker/ChangePassword.java
+++ src/main/java/com/labbooker/labbooker/ChangePassword.java
@@ -2,20 +2,71 @@ package com.labbooker.labbooker;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class ChangePassword {
 
-    public Button resetBtn;
+    @FXML
+    private Button resetBtn;
+
+    @FXML
+    private PasswordField passwordField1;
+
+    @FXML
+    private PasswordField passwordField2;
+
     @FXML
     private Button exitBtn;
 
 
     public void handleResetBtn(){
-        LecturerLogin lec = new LecturerLogin();
+        Alert alert;
 
-        System.out.println(getLecturerData.email);
+
+
+        if( passwordField1.getText().isBlank() == false && passwordField2.getText().isBlank() == false){
+
+            if(Objects.equals(passwordField1.getText(), passwordField2.getText())){
+                System.out.println("done");
+
+            }
+            else{
+
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error message");
+                alert.setHeaderText(null);
+                alert.setContentText("The passwords do not match!!");
+                alert.showAndWait();
+            }
+
+        }
+        else{
+
+            if(passwordField1.getText().isBlank() == true){
+
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please enter password in first field");
+                alert.showAndWait();
+            }
+            else if(passwordField2.getText().isBlank() == true){
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please enter password in second field");
+                alert.showAndWait();
+            }
+
+        }
+
+
+//        System.out.println(getLecturerData.email);
     }
 
     public void handleExitButtonClick(ActionEvent actionEvent) {
--- src/main/java/com/labbooker/labbooker/HelloApplication.java
+++ src/main/java/com/labbooker/labbooker/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcome.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("changePassword.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         String css = this.getClass().getResource("styles.css").toExternalForm();
