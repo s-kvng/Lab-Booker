8b534708eb903aff5528843568e2817daba065b4
s-kvng <kvngnathan8420@gmail.com>
2023-08-18 11:25:11 +0000

    change lab name input in booking form

--- src/main/java/com/labbooker/labbooker/BookingForm.java
+++ src/main/java/com/labbooker/labbooker/BookingForm.java
@@ -1,16 +1,30 @@
 package com.labbooker.labbooker;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BookingForm implements Initializable {
+
+    @FXML
+    private ComboBox<String> labNameComboBox;
+
+    @FXML
+    private TextField className;
+
+    @FXML
+    private DatePicker date;
+
+    @FXML
+    private TextField time;
 
     @FXML
     private Button exitBtn;
@@ -19,6 +33,33 @@ public class BookingForm implements Initializable {
     private Label lecturerName;
 
 
+    private ObservableList<String> labName = FXCollections.observableArrayList(
+            "Lab 1" , "Lab 2" , "Lab 3"
+    );
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        if(getLecturerData.first_name != null ){
+
+            lecturerName.setText(getLecturerData.first_name + " " + getLecturerData.last_name);
+        }
+
+        labNameComboBox.setItems(labName);
+
+        labNameComboBox.setConverter(new StringConverter<String>() {
+            @Override
+            public String toString(String s) {
+                return s;
+            }
+
+            @Override
+            public String fromString(String s) {
+                return s;
+            }
+        });
+
+    }
 
 
 
@@ -36,9 +77,8 @@ public class BookingForm implements Initializable {
         stage.close();
     }
 
-        @Override
-        public void initialize(URL url, ResourceBundle resourceBundle) {
 
-            lecturerName.setText(getLecturerData.first_name + " " + getLecturerData.last_name);
-        }
+
+
+
 }
